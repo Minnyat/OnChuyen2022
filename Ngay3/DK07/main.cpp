a832a0ffea1f0643e947138572787109538d2f82
minhatcoder <minhnhat.coder@gmail.com>
2022-06-02 18:08:20 +0300

    them tinh tong dung mang

--- Ngay3/DK07/main.cpp
+++ Ngay3/DK07/main.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int mangnguyen[11];
+long long a [110];
+long long n, sum;
 int main()
 {
-    mangnguyen[1] = 31
-    mangnguyen[2] = 28
-    mangnguyen[14]= 100;
-    mangnguyen[13]= 50;
-    for (int i=0;i<=14;i++){
-        cout<<mangnguyen[i]<<" ";
+    cin>>n;
+    for (int i=1;i<=n;i++){
+        cin>>a[i];
     }
-
+    for (int i =1;i<=n;i++){
+        sum +=a[i];
+    }
+    cout<<sum;
     return 0;
 }
